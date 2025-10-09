8d9e91f087beea7caed53a303cf9aee9a9009bc5
Esteban Durante <estebandurante194@gmail.com>
2025-10-09 19:45:42 -0300

    refactor(ingredient) agregado stock al momento de buscar un ingrediente

--- src/api/models/DTO/request/ingredientRequestDTO.ts
+++ src/api/models/DTO/request/ingredientRequestDTO.ts
@@ -1,17 +1,16 @@
 interface IIngredientRequestDTO {
   name: string;
-  price: number;
+  unitId: number;
+  lossFactor: number;
 }
 
 export class IngredientRequestDTO implements IIngredientRequestDTO {
   name: string;
-  price: number;
   unitId: number;
   lossFactor: number;
 
-  constructor(name: string, price: number, unitId: number, lossFactor: number) {
+  constructor(name: string, unitId: number, lossFactor: number) {
     this.name = name;
-    this.price = price;
     this.unitId = unitId;
     this.lossFactor = lossFactor;
   }
