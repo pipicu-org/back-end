f1d5603b769b68918f37ae38d9a6fdd8f58ca4f0
Esteban Durante <estebandurante194@gmail.com>
2025-10-24 19:59:00 -0300

    refactor: refactorizado servicio de stockMovement y stockMovementResponseDTO

--- src/config/inject.ts
+++ src/config/inject.ts
@@ -211,7 +211,29 @@ export const lineRepository = new LineRepository(
   dbTransitionTypeRepository,
   lineMapper,
 );
-export const lineService = new LineService(lineRepository);
+
+export const stockMovementRepository = new StockMovementRepository(
+  dbStockMovementRepository,
+);
+export const unitRepository = new UnitRepository(dbUnitRepository, unitMapper);
+
+export const unitService = new UnitService(
+  unitRepository,
+  unitMapper,
+  AppDataSource,
+);
+
+export const ingredientService = new IngredientService(
+  ingredientRepository,
+  ingredientMapper,
+);
+
+export const stockMovementService = new StockMovementService(
+  stockMovementRepository,
+  stockMovementMapper,
+  unitService,
+  ingredientService,
+);
 
 // Services
 
@@ -224,19 +246,6 @@ export const clientService = new ClientService(clientRepository, clientMapper);
 export const productService = new ProductService(
   productRepository,
   productMapper,
-);
-
-export const unitService = new UnitService(unitRepository);
-
-export const ingredientService = new IngredientService(
-  ingredientRepository,
-  ingredientMapper,
-);
-export const stockMovementService = new StockMovementService(
-  stockMovementRepository,
-  stockMovementMapper,
-  unitService,
-  ingredientService,
 );
 
 export const orderService = new OrderService(
